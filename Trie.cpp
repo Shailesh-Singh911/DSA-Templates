2a71772eee1e524762ad49ed98b525512a20a879
Shailesh-Singh911 <83928126+Shailesh-Singh911@users.noreply.github.com>
2023-05-25 23:22:19 +0530

    Create Trie.cpp

---
+++ Trie.cpp
@@ -0,0 +1,42 @@
+class Node{
+    public:
+        Node* links[26];
+        bool flag = false;
+};
+
+class Trie {
+public:
+    Node* root;
+    Trie() {
+        root = new Node();
+    }
+    
+    void insert(string word) {
+        Node* node = root;
+        for(int i = 0; i < word.size(); i++){
+            if(node -> links[word[i] - 'a'] == NULL){
+                node -> links[word[i] - 'a'] = new Node();
+            }
+            node = node -> links[word[i] - 'a'];
+        }
+        node -> flag = true;
+    }
+    
+    bool search(string word) {
+        Node* node = root;
+        for(int i = 0; i < word.size(); i++){
+            if(node -> links[word[i] - 'a'] == NULL) return false;
+            node = node -> links[word[i] - 'a'];
+        }
+        return node -> flag;
+    }
+    
+    bool startsWith(string prefix) {
+        Node* node = root;
+        for(int i = 0; i < prefix.size(); i++){
+            if(node -> links[prefix[i] - 'a'] == NULL) return false;
+            node = node -> links[prefix[i] - 'a'];
+        }
+        return true;
+    }
+};
